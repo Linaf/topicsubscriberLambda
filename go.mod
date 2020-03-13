0a971d601ee1a491c08407d65fe020523f0ce3e3
Lina Kebede <lkebede@gogoair.com>
2020-03-12 23:33:41 -0600

    Lambda triggered by SNS topic

---
+++ go.mod
@@ -0,0 +1,8 @@
+module github.com/Linaf/topicsubscriberLambda
+
+go 1.13
+
+require (
+	github.com/aws/aws-lambda-go v1.15.0
+	github.com/sirupsen/logrus v1.4.2
+)
---
+++ main.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"context"
+	"encoding/json"
+	"github.com/aws/aws-lambda-go/events"
+	"github.com/aws/aws-lambda-go/lambda"
+	log "github.com/sirupsen/logrus"
+	"os"
+)
+
+type Message struct {
+	Id          string `json:"id"`
+	FirstName      string `json:"firstName"`
+	LastName int64  `json:"lastName"`
+}
+func handler(c context.Context, e events.SNSEvent) {
+	log.Info("Started  LRU Raw log extractor lambda ...\n")
+	for _, record := range e.Records {
+		snsRecord := record.SNS
+
+		FirstName, ok := snsRecord.MessageAttributes["FirstName"].(map[string]interface{})
+		if ok && "pcp" == FirstName["Value"] {
+			log.Infof("FirstName: %s\n", FirstName["Value"])
+			message := snsRecord.Message
+			var msg = Message{}
+			err := json.Unmarshal([]byte(message), &msg)
+			if err != nil {
+				log.Errorf("Error Unmarshalling message %v", err.Error())
+			}
+			log.Infof("Message passed %v", msg)
+
+		}
+
+	}
+}
+func main() {
+	cloudenv := os.Getenv("CLOUD_ENVIRONMENT")
+	log.Infof("Started Topic subscriber lambda in %s", cloudenv)
+	lambda.Start(handler)
+}
